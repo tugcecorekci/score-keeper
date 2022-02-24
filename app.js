6a042b8efbdf9dc573abb927825ff449041e6b26
tugcecorekci <tugcecorekcioglu@gmail.com>
2022-02-24 10:34:32 +0100

    Updated players as objects and revised functions in app.js

--- app.js
+++ app.js
@@ -1,50 +1,45 @@
-const p1Btn = document.querySelector('#p1Btn')
-const p2Btn = document.querySelector('#p2Btn')
-const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p2Display')
-const resetBtn = document.querySelector('#reset')
-const playSelect = document.querySelector('#playto')
+const p1 = {
+    button: document.querySelector('#p1Btn'),
+    display: document.querySelector('#p1Display'),
+    score: 0
+}
 
-let p1Score = 0
-let p2Score = 0
+const p2 = {
+    button: document.querySelector('#p2Btn'),
+    display: document.querySelector('#p2Display'),
+    score: 0
+}
+
+const resetButton = document.querySelector('#reset')
+const playSelect = document.querySelector('#playto')
 let winScore = 3
 
+function updateScores(player, opponent) {
+    if (player.score != winScore) {
+        player.score += 1
+        player.display.textContent = player.score
+        if (player.score == winScore) {
+            player.button.disabled = true
+            opponent.button.disabled = true
+        }
+    }
+}
+
+function reset() {
+    p1.score = 0
+    p2.score = 0
+    p1.display.textContent = 0
+    p2.display.textContent = 0
+    p1.button.disabled = false
+    p2.button.disabled = false
+}
 
 playSelect.addEventListener('change', function () {
-    // winScore = parseInt(this.value)
     winScore = this.value
     reset()
 })
 
-p1Btn.addEventListener('click', () => {
-    if (p1Score != winScore) {
-        p1Score += 1
-        p1Display.textContent = p1Score
-        if (p1Score == winScore) {
-            p1Btn.disabled = true
-            p2Btn.disabled = true
-        }
-    }
-})
+resetButton.addEventListener('click', reset)
 
-p2Btn.addEventListener('click', () => {
-    if (p2Score != winScore) {
-        p2Score += 1
-        p2Display.textContent = p2Score
-        if (p2Score == winScore) {
-            p1Btn.disabled = true
-            p2Btn.disabled = true
-        }
-    }
-})
-
-resetBtn.addEventListener('click', reset)
-
-function reset() {
-    p1Score = 0
-    p2Score = 0
-    p1Display.textContent = 0
-    p2Display.textContent = 0
-    p1Btn.disabled = false
-    p2Btn.disabled = false
-}
+p1.button.addEventListener('click', () => updateScores(p1, p2))
+p2.button.addEventListener('click', () => updateScores(p2, p1))
